04cccb43f85d8429aec8d590728bcf524f9a80e6
joashneves <joashneves@gmail.com>
2025-01-07 21:53:56 -0300

    colocnado teste

--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
 # Comando para baixar as depedencias e inicar o bot
 
+### Ao clonar o repositorio com python baixado
+- O primeiro comando
 ```bash
 pip install -e .
 ```
-
+- O segundo comando (inicia o bot)
 ```bash
 discord-bot
 ```
---
+++ tests/conftest.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+import pytest_asyncio
+import discord
+import discord.ext.commands as commands
+import discord.ext.test as dpytest
+from discord.client import _LoopSentinel
+
+
+@pytest_asyncio.fixture
+async def bot(request):
+    intents = discord.Intents.default()
+    intents.members = True
+    intents.message_content = True
+    b = commands.Bot(command_prefix="!",
+                     intents=intents)
+    # set up the loop
+    if isinstance(b.loop, _LoopSentinel):
+        await b._async_setup_hook()
+
+    marks = request.function.pytestmark
+    mark = None
+    for mark in marks:
+        if mark.name == "cogs":
+            break
+
+    if mark is not None:
+        for extension in mark.args:
+            await b.load_extension("tests.internal." + extension)
+
+    dpytest.configure(b)
+    return b
+
+
+@pytest_asyncio.fixture(autouse=True)
+async def cleanup():
+    yield
+    await dpytest.empty_queue()
+
+
+def pytest_sessionfinish(session, exitstatus):
+    """ Code to execute after all tests. """
+
+    # dat files are created when using attachements
+    print("\n-------------------------\nClean dpytest_*.dat files")
+    file_list = Path('.').glob('dpytest_*.dat')
+    for file_path in file_list:
+        try:
+            file_path.unlink()
+        except Exception:
+            print("Error while deleting file : ", file_path)
