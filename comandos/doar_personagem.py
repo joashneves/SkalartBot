2608ef26766d5d2c2578159a31ffb584f2371dbb
joashneves <joashneves@gmail.com>
2025-03-28 03:46:22 -0300

    colocar troca

--- comandos/doar_personagem.py
+++ comandos/doar_personagem.py
@@ -1,12 +1,39 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+import asyncio
+from asyncio import TaskGroup
 import models
 from models.Obter_personagem import Manipular_Personagem
 class DoarPersonagem(commands.Cog):
     def __init__(self, bot: commands.bot):
         super().__init__()
         self.bot = bot
+        self.troca = {}
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message):
+        if message.author.bot:
+            return
+        print(f"VAR : {self.troca}")
+        if message.author.id in self.troca:
+            if message.content.startswith("sim"):
+                await message.channel.send("A troca foi aceita")
+            elif message.content.startswith("não"):
+                await message.channel.send("A troca foi recusada")
+            else:
+                print("Mensagem não é de troca")
+
+    async def temporizador(self, msg, id_novo_dono, interaction,  sleeptime):
+        print(f"AVISO : temporizador iniciado com {sleeptime} segundos e self.mensagem {self.troca[id_novo_dono]}")
+        await asyncio.sleep(sleeptime)
+        if id_novo_dono in self.troca[id_novo_dono]:
+            if self.troca[id_novo_dono] == []:
+                return
+            self.troca[id_novo_dono] = []
+            await interaction.responde.send_message("acabou o tempo de troca")
+
+
 
     @app_commands.command(name="doar_personagem", description="Troca um personagem com alguem")
     @app_commands.describe(nome="Nome do personagem que voce quer enviar")
@@ -17,9 +44,19 @@ class DoarPersonagem(commands.Cog):
                                   franquia: str,
                                   user: discord.Member):
         personagem = Manipular_Personagem.Obeter_um_personagem(interaction.guild.id, nome, franquia)
+        id_dono_antigo = interaction.user.id
+        id_dono_novo = user.id
+        guild_id = interaction.guild.id
+
         if not personagem:
             return
-        await interaction.response.send_message(f"{nome}, {franquia}, {user}, {personagem}")
+        await interaction.response.send_message(f"Troca iniciada, responda com sim ou não{nome}, {franquia}, {user}, {personagem}")
+        msg = interaction.id
+        self.troca[id_dono_novo] = ([id_dono_antigo, id_dono_novo, guild_id, nome, franquia])
+        print(f"VAR : {self.troca[id_dono_novo]}")
+        async with TaskGroup() as group:
+            group.create_task(self.temporizador(msg, id_dono_novo, interaction, 26))
+
 
     @doar_personagem.autocomplete('nome')
     async def doar_personagem_autocomplete(self,interact: discord.Interaction, pesquisa:str):
